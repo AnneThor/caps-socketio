d03fd7f48548895c945dbf3f8d1e1a638e3eddef
AnneThor <anne.thor@gmail.com>
2021-03-31 22:38:27 -0700

    finished functionality basics

--- caps.js
+++ caps.js
@@ -2,28 +2,48 @@
 
 require('dotenv').config();
 
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+const io = require('socket.io')(port);
 
 const caps = io.of('/caps');
 
-caps.on('connection', client => {
-    
-    socket.on('pickup', log);
-    socket.on('in-transit', log);
-    socket.on('delivered', log);
+caps.on('connection', socket => {
+
+  // when vendors/drivers join they need to go to own room
+  socket.on('join', (room) => {
+    socket.join(room);
+  })
+
+  socket.on('pickup', (payload, event) => {
+    log(payload, event);
+    socket.broadcast.emit('pickup', payload, event);
+  });
+
+  // this should only go back to pickup driver
+  socket.on('in-transit', (payload, event, driver) => {
+    log(payload, event);
+    io.of('caps').emit('in-transit', payload, event);
+    let storeId = payload.storeId;
+    socket.to(storeId).emit('in-transit', payload, event);
+  });
+
+  // this should only go back to vendor
+  socket.on('delivered', (payload, event) => {
+    log(payload, event);
+    let storeId = payload.storeId;
+    // socket.broadcast.emit('delivered', payload, event);
+    socket.to(storeId).emit('delivered', payload, event);
+  });
 
 })
 
 function log(payload, event) {
     let obj = {
         event: event,
-        time: new Date().getTime().toString();
-        payload: paylod
+        time: new Date().getTime().toString(),
+        payload: payload
     }
     console.log("Event", obj);
 }
-
-server.listen(process.env.port || 3000);
 
 module.exports = log;
